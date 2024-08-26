8ae503492eb5ae7a6fa549fd57c3caecc566492b
Remy CASTRO <remycastro27@icloud.com>
2024-08-26 18:55:06 +0200

    Fix text

--- src/app/(main)/agence-communication/page.tsx
+++ src/app/(main)/agence-communication/page.tsx
@@ -27,7 +27,7 @@ const MamatteExample: ComCustomerExample = {
 
 const BoucherieWiotte: ComCustomerExample = {
     from: "5000",
-    to: "11500",
+    to: "11.500",
     name: "Boucherie wiotte",
     description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
     logo: LogoWiotte as any,
